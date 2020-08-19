6999ab17138f641943f982a8e1a0fd901fa59a2c
jayasooriya-s <sjsooriya96@gmail.com>
2020-08-19 20:50:08 +0530

    views created for category using viewsets and urls.py created by router from rest framework and tested on local host

--- api/category/views.py
+++ api/category/views.py
@@ -4,6 +4,6 @@ from .models import Category
 # Create your views here.
 
 
-class CategoryViewSet(viewsets, ModelViewSet):
-    queryset = CategorySerializer.objects.all().order_by('name')
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all().order_by('name')
     serializer_class = CategorySerializer
