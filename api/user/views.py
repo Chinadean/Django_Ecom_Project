eb709829dc78e7abf0dacf4cd9842798723c3221
jayasooriya-s <sjsooriya96@gmail.com>
2020-08-20 19:34:20 +0530

    signin request added on views for user and validation also addsed

--- api/user/views.py
+++ api/user/views.py
@@ -1,7 +1,58 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from .serializers import UserSerializer
+from .models import CustomUser
+from django.http import JsonResponse
+from django.contrib.auth import get_user_model
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import login, logout
 import random
+import re
 # Create your views here.
 
 
 def generate_session_token(length=10):
     return ''.join(random.SystemRandom().choice([chr(i) for i in range(97, 123)]+[str(i) for i in range(10)]) for _ in range(length))
+
+
+@csrf_exempt
+def signin(request):
+    if not request.method == 'POST':
+        return JsonResponse({'error': 'Send a post requestwith valid parameter'})
+
+    username = request.POST['email']
+    password = request.POST['password']
+
+# Validation part
+
+    if not re.match("([\w\.\-_]+)?\w+@[\w-_]+(\.\w+){1,}", username):
+        return JsonResponse({'error': 'Enter valid email'})
+
+    if len(password) < 3:
+        return JsonResponse({'error': 'Password atleast of 3 letter'})
+
+    UserModel = get_user_model()
+
+    try:
+        user = UserModel.objects.get(email=username)
+
+        if user.check_password(password):
+            usr_dict = UserModel.objects.filter(
+                email=username).values().first()
+            usr_dict.pop('password')
+
+            if user.session_tiken != "0":
+                user.session_token = "0"
+                user.save()
+                return JsonResponse({'error': 'previous session exists!'})
+
+            token = generate_session_token()
+            user.session_token = token
+            user.save()
+            login(request, user)
+            return JsonResponse({'error': token, 'user': usr_dict})
+        else:
+            return JsonResponse({'error': 'Invalid Password'})
+
+    except UserModel.DoesNotExist:
+        return JsonResponse({'error': 'invalid email'})
